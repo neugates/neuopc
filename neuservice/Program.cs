d3e4bb479cae7e7521a4484fb02db303152741d5
gc87 <sfme@qq.com>
2023-05-17 23:37:16 +0800

    feat: add neuservice project

---
+++ neuservice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace neuservice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
